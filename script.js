9c435502eb3b04c7a6b5c3c9f9a706b61af8cea5
Accountingmadesimple <accomadesimple101@gmail.com>
2024-10-20 17:11:14 +0000

    Update script.js

--- script.js
+++ script.js
@@ -11,10 +11,10 @@ document.getElementById('quizForm').addEventListener('submit', function(event) {
 
     // Correct answers for each question
     const correctAnswers = {
-        q1: 'B',
-        q2: 'C',
+        q1: 'A',
+        q2: 'B',
         q3: 'B',
-        q4: 'C',
+        q4: 'A',
         q5: 'A'
     };
 
